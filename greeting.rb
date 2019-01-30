42d46026e2f06dd5faf0a7ec44700dada7976f0d
Matteo Muscas <thatman.j@gmail.com>
2019-01-30 11:34:36 +0100

    Delete greeting.rb

--- greeting.rb
+++
@@ -1,8 +0,0 @@
-def ciao(name, job ="minca")
-  puts "Ciao #{name}, conch\'e #{job}."
-end
-
-def greeting(language="Tua mamma", name)
-  puts "Hello, #{name}. We heard you are a great #{language} programmer."
-end
-
